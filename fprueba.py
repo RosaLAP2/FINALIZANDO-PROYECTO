0d4ed51521023986520cca6d1d1892fcaeb9cf69
deckarddtx <116812819+deckarddtx@users.noreply.github.com>
2022-12-19 07:51:38 -0500

    Update fprueba.py

--- fprueba.py
+++ fprueba.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
+#import seaborn as sns
 import matplotlib.pyplot as plt
 st.write("""PROYECTO FINAL""")
 st.write("""Universidad Nacional de San Agustín de Arequipa
